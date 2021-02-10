60159d070452f613d8f36bd8cfc8716742b699d1
TrueWarg <grazashar@mail.ru>
2021-02-10 22:28:25 +0200

    Change coco_dataset

--- dataset/coco_dataset.py
+++ dataset/coco_dataset.py
@@ -4,21 +4,20 @@ from typing import Tuple, List
 import cv2
 from torch.utils.data import Dataset
 
-DATASET_IMAGES_PATH = ''
-
 
 class CocoDataset(Dataset):
-    def __init__(self, images: List, annotations: List, transformer=None):
+    def __init__(self, images_path: str, images: List, annotations: List, transformer=None):
         self._transformer = transformer
         self._images = images
         self._annotations = annotations
+        self._images_path = images_path
 
     def __getitem__(self, index: int) -> Tuple:
         item = self._images[index]
         file_name = item['file_name']
         image_id = item['id']
 
-        image = cv2.imread(os.path.join(DATASET_IMAGES_PATH, file_name))
+        image = cv2.imread(os.path.join(self._images_path, file_name))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         selection = []
