f1d7f66b5aa831134460f8b6b7b341cfebc71b4f
TrueWarg <grazashar@mail.ru>
2021-02-13 13:04:59 +0200

    Add draft train logic

--- train.py
+++ train.py
@@ -1,10 +1,41 @@
+import torch.optim as optim
+import torch
+import os
+
+from model.ssd_network import SSDNetwork
 from torchvision import models
+from torch import nn
+from torch.utils.data import DataLoader
+from dataset.coco_dataset import CocoDataset
+
+DEVICE = device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def train():
+def train_epoch(model: nn.Module, optimizer: optim.Optimizer, data_loader: DataLoader) -> nn.Module:
+    model.train()
     pass
 
 
+def train(args):
+    os.makedirs(f'{args.checkpoint_dir}', exist_ok=True)
+
+    model = SSDNetwork(resnet34=models.resnet34(pretrained=True))
+    model.to(DEVICE)
+
+    # todo add scheduler for optimiser later
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
+
+    # todo add batch sampler
+    dataset = CocoDataset(images_path='', images=[], annotations=[])
+    data_loader = DataLoader(dataset=dataset, batch_size=8, shuffle=True)
+
+    for epoch in range(args.epoch_count):
+        model = train_epoch(model, optimizer, data_loader)
+
+    torch.save(model, f'{args.checkpoint_dir}/model.pt')
+
+
+# todo add some arg parser
 if __name__ == '__main__':
     resnet34 = models.resnet34(pretrained=False)
     print(resnet34)
