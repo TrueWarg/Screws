65a3f8e3163b1d4079a35c252ab202670de56755
TrueWarg <grazashar@mail.ru>
2021-06-24 21:08:16 +0200

    Add base transformes

--- dataset/augmentation/transforms.py
+++ dataset/augmentation/transforms.py
@@ -1,15 +1,72 @@
-from torchvision import transforms
+from typing import List
+
+import cv2
+import numpy as np
+import torch
 
 
-class DefaultTransform(object):
-    # use some extra config id needed
-    def __init__(self, image_size, mean, std):
-        self.transform = transforms.Compose([
-            transforms.Resize(image_size),
-            transforms.GaussianBlur(kernel_size=3),
-            transforms.Normalize(mean=mean, std=std),
+class TrainTransform:
+    def __init__(self, size: int, mean=0.0, std=1.0):
+        self._mean = mean
+        self._size = size
+        self._composed = Compose([
+            ConvertFromInts(),
+            ToPercentCoordinates(),
+            Resize(self._size),
+            SubtractMeans(self._mean),
+            lambda img, boxes=None, labels=None: (img / std, boxes, labels),
+            ToTensor(),
         ])
 
-    def __call__(self, image, boxes, category_ids):
-        # todo mage transformations for boxes also! torchvision.transforms has no such functionality
-        return self.transform(image)
+    def __call__(self, img, boxes, labels):
+        return self._composed(img, boxes, labels)
+
+
+class ToTensor(object):
+    def __call__(self, cvimage, boxes=None, labels=None):
+        return torch.from_numpy(cvimage.astype(np.float32)).permute(2, 0, 1), boxes, labels
+
+
+class Resize(object):
+    def __init__(self, size=300):
+        self.size = size
+
+    def __call__(self, image, boxes=None, labels=None):
+        image = cv2.resize(image, (self.size, self.size))
+        return image, boxes, labels
+
+
+class SubtractMeans(object):
+    def __init__(self, mean):
+        self.mean = np.array(mean, dtype=np.float32)
+
+    def __call__(self, image, boxes=None, labels=None):
+        image = image.astype(np.float32)
+        image -= self.mean
+        return image.astype(np.float32), boxes, labels
+
+
+class ToPercentCoordinates(object):
+    def __call__(self, image, boxes=None, labels=None):
+        height, width, channels = image.shape
+        boxes[:, 0] /= width
+        boxes[:, 2] /= width
+        boxes[:, 1] /= height
+        boxes[:, 3] /= height
+
+        return image, boxes, labels
+
+
+class ConvertFromInts(object):
+    def __call__(self, image, boxes=None, labels=None):
+        return image.astype(np.float32), boxes, labels
+
+
+class Compose(object):
+    def __init__(self, transforms: List):
+        self._transforms = transforms
+
+    def __call__(self, image, boxes=None, labels=None):
+        for t in self._transforms:
+            image, boxes, labels = t(image, boxes, labels)
+        return image, boxes, labels
